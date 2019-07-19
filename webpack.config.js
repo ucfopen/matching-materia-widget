603ff3c26cb441fdaa9f9968e5fdf1ebb7b19232
Davis Goff <davis.goff@ucf.edu>
2019-07-19 11:05:30 -0400

    Remove yarn-error.log and update entries for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const srcPath = path.join(__dirname, 'src') + path.sep
 const outputPath = path.join(__dirname, 'build') + path.sep
 const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
 
-const entries = widgetWebpack.getDefaultEntries()
 const copy = widgetWebpack.getDefaultCopyList()
 
 // Append the new items we want copied
@@ -13,25 +12,35 @@ copy.push({
 	to: outputPath,
 })
 
-entries['creator.js'] = [
-	path.join(srcPath, 'modules', 'matching.coffee'),
-	path.join(srcPath, 'controllers', 'creator.coffee'),
-	path.join(srcPath, 'directives', 'audioControls.coffee'),
-	path.join(srcPath, 'directives', 'focusMe.coffee'),
-	path.join(srcPath, 'directives', 'ngEnter.coffee'),
-	path.join(srcPath, 'directives', 'inputStateManager.coffee')
-]
-
-entries['player.js'] = [
-	path.join(srcPath, 'modules', 'matching.coffee'),
-	path.join(srcPath, 'controllers', 'player.coffee'),
-	path.join(srcPath, 'directives', 'audioControls.coffee')
-]
-
-entries['guides/player.temp.html'] = [
-	path.join(srcPath, '_guides', 'player.md'),
-	path.join(srcPath, '_guides', 'creator.md')
-]
+const entries = {
+	'creator.js': [
+			path.join(srcPath, 'modules', 'matching.coffee'),
+			path.join(srcPath, 'controllers', 'creator.coffee'),
+			path.join(srcPath, 'directives', 'audioControls.coffee'),
+			path.join(srcPath, 'directives', 'focusMe.coffee'),
+			path.join(srcPath, 'directives', 'ngEnter.coffee'),
+			path.join(srcPath, 'directives', 'inputStateManager.coffee')
+	],
+	'player.js': [
+			path.join(srcPath, 'modules', 'matching.coffee'),
+			path.join(srcPath, 'controllers', 'player.coffee'),
+			path.join(srcPath, 'directives', 'audioControls.coffee')
+	],
+	'creator.css': [
+			path.join(srcPath, 'creator.html'),
+			path.join(srcPath, 'creator.scss')
+	],
+	'player.css': [
+			path.join(srcPath, 'player.html'),
+			path.join(srcPath, 'player.scss')
+	],
+	'guides/player.temp.html': [
+			path.join(srcPath, '_guides', 'player.md')
+	],
+	'guides/creator.temp.html': [
+			path.join(srcPath, '_guides', 'creator.md')
+	]
+}
 
 const customCopy = copy.concat([
 	{
