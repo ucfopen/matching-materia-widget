efb3270a2b4012d60cc0f71d75106130c24a1427
Davis Goff <davis.goff@ucf.edu>
2019-07-19 10:23:05 -0400

    Update how helper docs are added and remove unnecessary dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const marked = require('meta-marked')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const srcPath = path.join(__dirname, 'src') + path.sep
 const outputPath = path.join(__dirname, 'build') + path.sep
 const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
@@ -30,12 +28,15 @@ entries['player.js'] = [
 	path.join(srcPath, 'directives', 'audioControls.coffee')
 ]
 
-entries['guides/guideStyles.css'] = [srcPath+'_helper-docs/guideStyles.scss']
+entries['guides/player.temp.html'] = [
+	path.join(srcPath, '_guides', 'player.md'),
+	path.join(srcPath, '_guides', 'creator.md')
+]
 
 const customCopy = copy.concat([
 	{
-		from: `${srcPath}/_helper-docs/assets`,
-		to: `${outputPath}/guides/assets`,
+		from: path.join(srcPath, '_guides', 'assets'),
+		to: path.join(outputPath, 'guides', 'assets'),
 		toType: 'dir'
 	}
 ])
@@ -46,22 +47,6 @@ const options = {
 	entries: entries
 }
 
-const generateHelperPlugin = name => {
-	const file = fs.readFileSync(path.join(__dirname, 'src', '_helper-docs', name+'.md'), 'utf8')
-	const content = marked(file)
-
-	return new HtmlWebpackPlugin({
-		template: path.join(__dirname, 'src', '_helper-docs', 'helperTemplate'),
-		filename: path.join(outputPath, 'guides', name+'.html'),
-		title: name.charAt(0).toUpperCase() + name.slice(1),
-		chunks:['guides'],
-		content: content.html
-	})
-}
-
 let buildConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
-
-buildConfig.plugins.unshift(generateHelperPlugin('creator'))
-buildConfig.plugins.unshift(generateHelperPlugin('player'))
 
 module.exports = buildConfig
